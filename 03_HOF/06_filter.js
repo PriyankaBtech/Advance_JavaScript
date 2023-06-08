6488f69af7efbc4048bcf7f23c53dc933e604481
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-06-08 16:44:12 +0530

    filter

--- 03_HOF/06_filter.js
+++ 03_HOF/06_filter.js
@@ -1,5 +1,24 @@
 /*********************** filter *****************************/
 
+/*
+The callback function can be passed to the filter function in the following ways
+1.  Function declaration
+2.  Arrow Function
+3.  Callback Function.
+
+
+// Function Declaration
+array.filter(function (item, index, array) {}
+
+// Arrow Function
+array.filter((item, index, array) => {}
+
+// Call back Function
+array.filter(callback)
+*/
+
+
+
 let fruits = ["apple", "Mango", "orange", "banana", "cherry"]
 
 let singleFruit = fruits.filter((element) => {
@@ -11,8 +30,21 @@ console.log(singleFruit); // [ 'Mango' ]
 // if you want to print particular thinks from the array we use filter() function not map.
 // return an Array
 
-// Example 2 
-// print the element which are divisble by 10
 
-let numbers = [19, 20, 30, 4, 50, 334, 6, 100]
+// example
+const heros = ["naagraj", "doga", "dhruva", "maniraj"]
+
+const herosWithRaj = heros.filter((h) => {
+    return h.endsWith('raj')
+    // without return => []
+})
+console.log(herosWithRaj) // [ 'naagraj', 'maniraj' ]
+
+
+
+// another example
+let userNames = ["Mithun", "Anurag", "Alka", "Prabir", "Vinay"]
+
+let longUserNames = userNames.filter((item) => item.length > 5)
+console.log(longUserNames); // OUTPUT : [ 'Mithun', 'Anurag', 'Prabir' ]
 
