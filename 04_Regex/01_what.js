3a5c443869786281d74864ec01d7609f8849328d
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-06-15 15:09:08 +0530

    What is Regex

---
+++ 04_Regex/01_what.js
@@ -0,0 +1,16 @@
+/**************************** Regex *******************************/
+
+
+// Regex is powerful concept as used by developers,  we can used regex to match the specific character, words and even the entire string of the text documents.
+
+// They are generally used in text editor and programming languages for search, replace and data validation.
+
+// Regex pattern are made up of a combination of characters and special symbols
+
+// Basic components of Regex :
+/*
+1. Opening Tag 
+2. Closing Tag
+3. Pattern 
+4. Flag
+*/
