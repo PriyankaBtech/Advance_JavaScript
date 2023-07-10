60576ce1517e6b4a103a1eb7c6925d76785765cd
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-07-10 14:26:38 +0530

    practice of functions

--- 02_Functions/revision.js
+++ 02_Functions/revision.js
@@ -74,5 +74,53 @@ const user = {
     id: "12345"
 }
 
+function addUser(obj) {
+    return `A user name ${obj.name} got an email ${obj.email}`  
+    // wrong => user.name, this.name  
+}
+console.log(addUser(user)) // A user name Priyanka got an email priyanka@gmail.come
+
+
+
+// Normal function => function function_name() {}
+function addTwoNumber(num1, num2) {
+    return num1 + num2    
+}
+
+// Arrow Function => () => {}
+const addTwoNumber2 = (num1, num2) => {
+    return num1 + num2
+}
+
+// if code is only of single line then you can remove the {}
+const addTwoNumber3 = (num1, num2) => num1 + num2
+
+// single parameter
+const addTwoNumber4 = num => 5 // implicit return (means return apply automatically)
+
+
+//{email: "rahul@gmail.com"} // How get this type(object) of Output ?
+const addTwoNumber5 = num => ({email: "rahul@gmail.com"})
+
+// for two parameters
+const addTwoNumber6 = (num1, num2) => ({email: "rahul@gmail.com"}) // implicit return
+
+
+
+// this
+function learning() {
+    console.log(this)    
+}
+
+const learningTwo = () => {
+    console.log(this)
+}
+
+learning()// Object [global]{} => with lot of values
+learningTwo() // {}
+
+// In Browser
+learning() // window{}
+learningTwo() // window{}
 
 
