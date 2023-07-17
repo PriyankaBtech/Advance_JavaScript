e0e8a14048ae80df60291adc84d0d32108490ec0
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-07-17 17:57:15 +0530

    learn await

---
+++ 09_Async_&_Await/02_await.js
@@ -0,0 +1,19 @@
+ // Await 
+
+ console.log("start")
+
+ async function printHelloAfterThreeSeconds() {
+    let data = new Promise((resolve, reject) => {
+    setTimeout(() => {
+    resolve("Printing: Hello")
+    }, 3000) })
+
+
+let result = await data // Wait until the asynchronous operation is resolved : 3 seconds
+    console.log(result)
+}
+printHelloAfterThreeSeconds() // Printing: Hello
+
+console.log("Namasty")
+console.log("End")
+
